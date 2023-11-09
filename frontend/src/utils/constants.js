74de15eeb71c28614575274a5bfed1040e506409
Julia Chelnokova <chelnokova.yuv@yandex.ru>
2023-11-09 18:24:45 +0700

    исправлена валидация имени

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,5 +1,5 @@
 export const patterns = {
-  name: "^[а-яА-ЯЁё\\s\\-]+$",
+  name: "^[a-zA-Zа-яёА-ЯЁs-]+$",
   email: "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$",
 };
 
--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,43 +1,6 @@
-// import { SHORT_FILM_DURATION } from "./constants";
-// import { useLocation } from "react-router-dom";
-// const location = useLocation();
-
 export function timeConverter(card) {
   const hours = Math.floor(card.duration / 60);
   const minutes = card.duration - hours * 60;
   const duration = hours ? `${hours}ч ${minutes}м` : `${minutes}мин`;
   return duration;
 }
-
-// export function filteredMovies(movies, dataQuery, checkbox, setNullResult) {
-//   const searchMovies = movies.filter((movie) => {
-//     const movieRu = movie.nameRU
-//       .toLowerCase()
-//       .includes(dataQuery.toLowerCase());
-//     const movieEn = movie.nameEN
-//       .toLowerCase()
-//       .includes(dataQuery.toLowerCase());
-
-//     return movieRu || movieEn;
-//   });
-//   if (checkbox) {
-//     const shortSearchMovies = searchMovies.filter((movie) => {
-//       return movie.duration < SHORT_FILM_DURATION;
-//     });
-//     if (shortSearchMovies.length === 0) {
-//       setNullResult(true);
-//     } else {
-//       setNullResult(false);
-//       localStorage.setItem("initialCards", JSON.stringify(shortSearchMovies));
-//       return shortSearchMovies;
-//     }
-//   } else {
-//     setNullResult(false);
-//     if (location.pathname === "/movies") {
-//       localStorage.setItem("initialCards", JSON.stringify(searchMovies));
-//     } else {
-//     }
-
-//     return searchMovies;
-//   }
-// }
